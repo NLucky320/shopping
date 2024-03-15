7b8f40bcc3d488b7a7bd0655ba6ccb99785e133c
Nlucky320 <luckyes144@gmail.com>
2024-03-15 15:08:10 +0600

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,33 @@ import { useEffect } from 'react'
 
 function App() {
   const [products, setProducts] = useState([])
+  const [cart, setCart]=useState([])
   
   useEffect(() => {
     fetch('../public/fakeData.json')
       .then(res => res.json())
     .then(data=>setProducts(data))
 },[])
-console.log(products)
+  // console.log(products)
+  
+  const handleCart = product => {
+    const isAdded = cart.find(item => item.id == product.id);
+    // console.log(isAdded)
+    if (!isAdded) {
+      setCart([...cart, product])
+    }
+    else{
+      alert('already added')
+    }
+}
+
+  const handleDelete = id => {
+    // console.log(id)
+    const newCart = cart.filter(item => item.id != id);
+    //   console.log(newCart)
+    setCart(newCart)
+  }
+  
   
   return (
     <>
@@ -22,19 +42,36 @@ console.log(products)
       <div className='max-w-[1170px] mx-auto items-center p-4'>
        <Header></Header>
 
-        <div className='flex justify-around p-4'>
-          <div>
+        <div className='md:flex justify-between gap-2'>
+          <div className='md:w-2/3'>
              {
-              products.map(product => <Products key={product.id} product={ product}></Products>)
+              products.map(product => <Products key={product.id} product={ product} handleCart={handleCart}></Products>)
             }
       </div>
-       
-          <div className='bg-base-200 rounded-lg shadow-sm w-1/3'>
-            <h1 className='text-4xl font-semibold text-center p-4'>This is cart</h1>
+          <div className='md:w-1/3'>
+                  <div className='bg-base-200 rounded-lg shadow-sm p-4'>
+            <div className='w-full'>
+                <h1 className='text-4xl font-semibold text-center p-4'>This is cart</h1>
+            <div className='flex justify-between gap-2'>
+              <h3>Name</h3>
+              <h3>Price</h3>
+              </div>
+              <div>
+                {
+                  cart.map((item,index) => (
+                    <div key={index} className='flex justify-between gap-2 space-y-4 items-center text-center'>
+                      <p>{ index+1}</p>
+                      <h3>{item.title}</h3>
+                      <h3>{ item.price}</h3>
+                      <button onClick={()=>handleDelete(item.id)}>Delete</button>
+                    </div>
+                  ))}
+              </div>
           </div>
+          </div>
+       </div>
+   
         </div>
-
-
       </div>
       
     </>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,8 +1,9 @@
 
-const Products = ({ product }) => {
-    console.log(product)
+const Products = ({ product, handleCart }) => {
+    // console.log(product)
+   
     return (
-        <div className=" w-2/3 pb-8">
+        <div className="pb-8">
             <div className="text-center items-center space-y-4"> 
                 <div className="w-[200px] mx-auto">
                      <img className="" src={product.image} alt="" />
@@ -10,10 +11,12 @@ const Products = ({ product }) => {
                 <h3 className="text-3xl font-bold">Title: {product.title}</h3>
                 <p className="">Description: {product.description} </p>
                 <p className="text-2xl font-medium">Price: {product.price} <span></span> $</p>
-                <button className="btn">Add to cart</button>
+                <button onClick={()=>{handleCart(product)}} className="btn">Add to cart</button>
             </div>
         </div>
     );
 };
+
+
 
 export default Products;
